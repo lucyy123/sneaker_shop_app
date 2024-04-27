14fd43fce7360b95f53e0361bdd9783f2a20f8ca
monis_khan <monisykhan@gmail.com>
2024-04-27 23:05:55 +0530

    added the drawer into home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -37,6 +37,81 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Builder(builder: (context) {
+          return Padding(
+            padding: const EdgeInsets.only(left: 10.0),
+            child: IconButton(
+                onPressed: () {
+                  print("on press button is pressed");
+                  Scaffold.of(context).openDrawer();
+                },
+                icon: Icon(
+                  Icons.menu,
+                  color: Colors.black,
+                )),
+          );
+        }),
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.grey[900],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(children: [
+              //!--------------  logo
+              DrawerHeader(
+                  child: Image.asset(
+                "assets/images/drawerLogo.png",
+                color: Colors.white,
+                width: double.maxFinite,
+              )),
+
+              //*-------------------------- other Pages
+
+              SizedBox(
+                height: 30,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0),
+                child: ListTile(
+                  title: Text(
+                    "Home",
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.w900),
+                  ),
+                  leading: Icon(Icons.home, color: Colors.white),
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0),
+                child: ListTile(
+                  title: Text(
+                    "About",
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.w900),
+                  ),
+                  leading: Icon(Icons.info, color: Colors.white),
+                ),
+              ),
+            ]),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, bottom: 20.0),
+              child: ListTile(
+                title: Text(
+                  "Logout",
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.w900),
+                ),
+                leading: Icon(Icons.logout_outlined, color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
       body: _pages[_selectedIndex],
       bottomNavigationBar: MyBottomBar(
         handlOnTabChange: (index) => handleNavigatingBar(index),
